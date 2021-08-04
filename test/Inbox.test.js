4bb62269643051be952165ccbd528de35544aa5a
Carlos Zuloaga <czuloaga@gmail.com>
2021-08-04 11:00:59 -0400

    Accounts creation

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,25 @@
 const assert=require('assert');
 const ganache=require('ganache-cli');
+//Library
 const Web3=require('web3');
+//Test network-web3 instance of Web3
 const web3=new Web3(ganache.provider());
+
+beforeEach(()=>{
+    //Get a list of all accounts
+web3.eth.getAccounts()
+    .then(fetchedAccounts=>{
+        console.log(fetchedAccounts);
+    });
+
+    //Use one of those accounts to deploy the contract
+});
+
+describe('Inbox',()=>{
+    it('deploys a contact',()=>{
+        
+    });
+});
 
 /*
 test example
