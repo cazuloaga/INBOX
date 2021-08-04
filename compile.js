99d9b3d95af41ddee3ad67428ae2c67e470adb9e
Carlos Zuloaga <czuloaga@gmail.com>
2021-08-04 17:32:01 -0400

    Changes

--- compile.js
+++ compile.js
@@ -5,4 +5,5 @@ const solc=require('solc');
 const inboxPath=path.resolve(__dirname,'contracts','Inbox.sol');
 const source=fs.readFileSync(inboxPath,"utf8");
 
+//2 properties assigned in particular: interface and bytecode.
 module.exports=solc.compile(source,1).contracts[':Inbox'];
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -72,3 +72,4 @@ describe('Car',()=>{
 });
 */
 
+//https://rinkeby.infura.io/v3/8c0db3bc80914db4a37aa04e2ddb0188
